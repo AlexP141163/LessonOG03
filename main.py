4496483410ff74f90a0e6970ab6a911e32792bd3
AlexP141163 <pilyugin.aa@gmail.com>
2024-03-26 11:37:50 +0300

    add final main.py

--- main.py
+++ main.py
@@ -2,18 +2,22 @@
 import pygame
 import random
 import time
+
 pygame.init() # Инициализация:
+
+# Переменные
 
 SCRIN_WIDTH = 800  # Переменные, задают ширину и высоту экрана в пикселях:
 SCRIN_HEIGHT = 600
+
 # Создпем переменную "screen", в которой будем хранить экран с заданными шириной и высотой экрана:
 screen = pygame.display.set_mode((SCRIN_WIDTH, SCRIN_HEIGHT))
+
 # Создаем заголовок экрана (set_caption - установить заголовок):
-pygame.display.set_caption("Игра Тир")
+pygame.display.set_caption("Игра Тир, Победа - 100 Очков")
 
 font = pygame.font.Font(None, 36) # Шривт для счета:
-score = 0                                     # Инициализация переменной для счета:
-i = 0  # Инициализация переменной цикла для счета:
+score = 0  # Инициализация переменной для счета:
 text = "Счет:"
 text_final = "Вы Победили!"
 # Загрузим иконку игры. Сохраним файл иконки в папкн "img":
@@ -29,8 +33,12 @@ target_height = 80
 # каритнка не выходила за пределы экрана и также по высоте по оси - У:
 target_x = random.randint(0, SCRIN_WIDTH - target_width)
 target_y = random.randint(0, SCRIN_HEIGHT - target_height)
-# Зададим Рамдомный цвет экрана:
-color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+target_speed_x = 0.5  # Скорость движения мишени по горизонтали "Включено для непрерывного движения"
+target_speed_y = 0.5  # Скорость движения мишени по вертикали   "Включено для непрерывного движения"
+
+# Зададим Рамдомный цвет экрана: Цвет экрана в новой версии будет постоянный:
+#color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+color = (0, 190, 180)
 
 # Условие цикла непрерывной работы программы. Как только переменная "running = false", программа
 # закончиться работой "pygame.quit()"
@@ -48,30 +56,40 @@ while running:
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCRIN_WIDTH - target_width)
                 target_y = random.randint(0, SCRIN_HEIGHT - target_height)
-                i += 10
-                score = i
+                score += 10
+
+    # Обновляем координаты мишени
+    target_x += target_speed_x      # В новой версии при движении мишени постоянно:
+    target_y += target_speed_y      # В новой версии при движении мишени постоянно:
+
+    # Проверяем, не вышла ли мишень за пределы экрана, и меняем направление движения, если это так:
+    if target_x + target_width > SCRIN_WIDTH or target_x < 0:
+        target_speed_x = -target_speed_x
+    if target_y + target_height > SCRIN_HEIGHT or target_y < 0:
+        target_speed_y = -target_speed_y
 
     # Отрисовываем на экране мишень  с координатами ("target_img, (target_x, target_y") оператором ("screen.blit"):
     screen.blit(target_img, (target_x, target_y))
 
     # Создание текстовой поверхности
-    text_surface = font.render(text, True, 0)
+    #text_surface = font.render(text, True, 0)
+    text_surface = font.render(f"{text} {score}", True, (0, 0, 0))
     # Отображение текста
     screen.blit(text_surface, (10, 20))
     # Создание текстовой поверхности с числовым значением, преобразованным в строку
-    text_surface = font.render(str(score), True, 0)
+    #text_surface = font.render(str(score), True, 0)
     # Отображение текста
-    screen.blit(text_surface, (80, 20))
+    #screen.blit(text_surface, (80, 20))
+    # Условие завершения игры - 100 Очков. Игра завершиться через 5 секунд.
     if score > 90:
-
         # Создание текстовой поверхности с числовым значением, преобразованным в строку
         text_surface = font.render(str(text_final), True, 0)
-
         # Отображение текста
         screen.blit(text_surface, (300, 280))
         pygame.display.update()
         time.sleep(5)  # Пауза на 5 секунд
         running = False
+
     pygame.display.update() # Обновление экрана, Обязательно вставляем со смещением для цикла "while":
 
 pygame.quit()
